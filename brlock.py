d5e8129d714dbea71c590348839065261a94fe13
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-05-02 15:30:18 +0200

    done. initially i misunderstood the concept of a big reader lock and ended up coding something very similar to a simple reader writer lock. fortunately i noticed it and now it's done right (i hope :))

--- brlock.py
+++ brlock.py
@@ -1,44 +1,75 @@
 #!/usr/bin/python3
 from threading import Thread, Lock, Condition, get_ident
 from random import randint
+from time import sleep
 
-# errors might occure if more than threads_count threads use the same BRLock
 class BRLock:
     def __init__(self, threads_count):
         self.threads_count = threads_count
         self.locks = [Lock() for i in range(threads_count)]
-        self.state = 0
-        # -1 means a writer is writing,
-        # 0 means no one is using the resource atm,
-        # >0 indicates the number of active readers
-        self.state_cond = Condition()
-        self.lock_vs_thread = {} #a dictionary identifying locks with threads
+        #a dictionary identifying locks with threads
+        self.lock_vs_thread = {i:0 for i in range(threads_count)}
+        self.lock_vs_thread_mutex = Lock()
+
+        self.another_writer_active = False;
+        # self.another_writer_mutex = Lock()
+
+        self.another_writer_cond = Condition()
+
 
     def rd_acquire(self):
-        with self.state_cond:
-            while self.state == -1:
-                self.state_cond.wait()
-            lock_id = randint(0, self.threads_count - 1)
-            self.locks[lock_id].acquire()
-            self.lock_vs_thread[lock_id] = get_ident() #gets the id of the current thread
-            self.state += 1
+        lock_id = randint(0, self.threads_count - 1)
+        self.locks[lock_id].acquire()
+        with self.lock_vs_thread_mutex:
+            #gets the id of the current thread
+            self.lock_vs_thread[lock_id] = "R{}".format(get_ident())
+            # print(self.info())
 
     def rd_release(self):
-        with self.state_cond:
+        with self.lock_vs_thread_mutex:
             for i in self.lock_vs_thread:
-                if self.lock_vs_thread[i] == get_ident():
+                if self.lock_vs_thread[i] == "R{}".format(get_ident()):
                     self.locks[i].release()
-                    self.state -= 1
-                    # ...
+                    self.lock_vs_thread[i] = 0
+                    return
+            print("wtf: there's no such lock-thread combination")
+            print("my thread_id: {}".format(get_ident()))
+            print(self.lock_vs_thread) #pretty print maybe?
 
     def wr_acquire(self):
-        pass
+        #sync with mutex and sleep(). a deadlock IS POSSIBLE with this solution!
+        # with self.another_writer_mutex:
+        #     print("wr_acquire started")
+        #     while self.another_writer_active == True:
+        #         print("sleeping")
+        #         sleep(1)
+        #     self.another_writer_active = True;
+
+        #sync with cond
+        with self.another_writer_cond:
+            while self.another_writer_active == True:
+                self.another_writer_cond.wait()
+            self.another_writer_active = True;
+
+        for i in range(len(self.locks)):
+            self.locks[i].acquire()
+            with self.lock_vs_thread_mutex:
+                self.lock_vs_thread[i] = "W{}".format(get_ident())
+                # print(self.info())
 
     def wr_release(self):
-        pass
+        for i in range(len(self.locks)):
+            self.locks[i].release()
+            with self.lock_vs_thread_mutex:
+                self.lock_vs_thread[i] = 0
+                # print(self.info())
 
-brlock = BRLock(8)
-thread = Thread(target=brlock.rd_acquire)
-thread.start()
-thread.join()
-print(brlock.lock_vs_thread)
+        # with self.another_writer_mutex:
+        #     self.another_writer_active = False;
+
+        with self.another_writer_cond:
+            self.another_writer_active = False;
+            self.another_writer_cond.notify()
+
+    def info(self):
+        return "lock:thread : {}".format(self.lock_vs_thread)
--- test.py
+++ test.py
@@ -1,17 +1,56 @@
 #!/usr/bin/python3
-from threading import Thread, Lock, get_ident
+from threading import Thread
+import brlock
+from random import randint
 
-def f():
-    print(get_ident())
+class Reader(Thread):
+    def __init__(self, id):
+        self.id = id;
+        super().__init__()
 
-# t = Thread(target=f)
-# t.start()
-# t.join()
+    def read(self):
+        global resource
+        print("reader {} reads: {}".format(self.id, resource))
 
-# l = [1,2,3,4]
-# print(l[0])
+    def run(self):
+        brl.rd_acquire()
+        self.read()
+        brl.rd_release()
 
-d = {1:"one",5:"five",60:"sixty", 2:"two", 3:"three"}
-# print(size(d))
-for i in d:
-    print(d[i])
+class Writer(Thread):
+    def __init__(self, id):
+        self.id = id;
+        super().__init__()
+        self.texts = ["happy-elephant", "agile-cat", "friendly-dog", "shady-rat", "dangerous-hornet", "wise-owl"]
+
+    def write(self):
+        brl.wr_acquire()
+        global resource
+        old_res = resource
+        resource = self.texts[randint(0,5)]
+        print("writer {} modified from \"{}\" to \"{}\"".format(self.id, old_res, resource))
+        brl.wr_release()
+
+    def run(self):
+        self.write()
+
+threads_count = 8
+brl = brlock.BRLock(threads_count)
+resource = "sample-text"
+
+readers = [Reader(i) for i in range(threads_count + 10)]
+writers = [Writer(i) for i in range(5)]
+threads = readers
+threads.insert(2,writers[0])
+threads.insert(3,writers[1])
+threads.insert(4,writers[2])
+threads.insert(6,writers[3])
+threads.insert(13,writers[4])
+print(threads)
+print()
+
+for i in range(len(threads)):
+    threads[i].start()
+
+for i in range(len(threads)):
+    threads[i].join()
